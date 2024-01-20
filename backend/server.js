13101c16cb7a7c1178a7ca9ffbdaba36eb1c5740
KevSchultz <ksschultz02@gmail.com>
2024-01-20 15:57:55 -0800

    updated backend to server static in build(must be created)

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 80;
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
-});
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
