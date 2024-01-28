6480e3e8e9a0c1ca0c88f62bb6f2590e966eade0
KevSchultz <ksschultz02@gmail.com>
2024-01-27 18:54:49 -0800

    1Din1D scaling and moving

--- frontend/src/sketches/oneInOneSketch.js
+++ frontend/src/sketches/oneInOneSketch.js
@@ -3,79 +3,111 @@
  * @author Kevin Schultz
  * @project Emergent Playground
  */
-import vertexShader from "../shaders/basic.vert";
-import fragmentShader from "../shaders/1Din1D.frag";
+import vertexShader from '../shaders/basic.vert';
+import fragmentShader from '../shaders/1Din1D.frag';
 
 const oneInOneSketch = (p) => {
+    // size dimension variables
+    let canvasWidth = window.innerWidth;
+    let canvasHeight = window.innerHeight;
+    let worldWidth = 100;
+    let worldHeight = 0.5;
+    let scalar = 25;
 
-  let shader;
-  let prevFrame;
-  let numberOfCellsHorizontal = 100;
-  let numberOfCellsVertical = 1;
-  let cellWidth;
-  let cellHeight;
+    // other sketch globals
+    let shader1Din1D;
+    let buffer;
+    let previousState;
+    let isFirstGeneration = true;
+    let bufferX = 0;
+    let bufferY = 0;
+    let previousMouseX = 0;
+    let previousMouseY = 0;
+    let backgroundColor = '#696969';
 
-  p.preload = () => {
-    shader = p.loadShader(vertexShader, fragmentShader);
-    p.frameRate(15);
-  };
+    p.preload = ()  => {
+        shader1Din1D = p.loadShader(vertexShader, fragmentShader);
+    };
 
-  p.setup = () => {
-    p.createCanvas(1000, 10, p.WEBGL);
-    p.background(0, 0, 0);
-    p.noStroke();
-    p.pixelDensity(1);
-    p.noSmooth();
-    
-    p.shader(shader);
+    p.setup = () => {
+        // main canvas used to render scaled image webgl buffer (canvas must also be webgl)
+        p.createCanvas(canvasWidth, canvasHeight);
+        p.noSmooth();
+        p.background(backgroundColor);
 
-    cellWidth = p.width / numberOfCellsHorizontal;
-    cellHeight = p.height / numberOfCellsVertical;
+        // create off-screen graphics buffer with webgl
+        buffer = p.createGraphics(worldWidth, worldHeight, p.WEBGL);
+        buffer.noSmooth();
+        p.frameRate(24);
 
-    prevFrame = p.createGraphics(p.width, p.height);
-    prevFrame.background(0, 0, 0);
-    prevFrame.noStroke();
-    prevFrame.pixelDensity(1);
-    prevFrame.noSmooth();
+        // set shader,resolution unifrom and draw white background to buffer
+        buffer.shader(shader1Din1D);
+        shader1Din1D.setUniform('resolution', 1.0 / worldWidth);
 
-  };
-
-  p.mouseClicked = () => {
-    // Check the color of the square at the mouse position in the previous frame
-    // If it's black, we draw white, otherwise we draw black.
-    let color = prevFrame.get(p.mouseX, p.mouseY);
-    if (color[0] === 0 && color[1] == 0 && color[2] == 0) {
-      prevFrame.fill(255, 255, 255);
-    } else {
-      prevFrame.fill(0, 0, 0);
+        // create off-screen graphics buffer to hold the previous state of the cellular automata
+        previousState = p.createGraphics(worldWidth, worldHeight);
+        previousState.noSmooth();
+        previousState.stroke(255);
+        previousState.line(0, 0, 0, 0);
     }
 
-    prevFrame.square(
-      Math.floor(p.mouseX / cellWidth) * cellWidth,
-      Math.floor(p.mouseY / cellHeight) * cellHeight,
-      cellWidth
-    );
-  }
+    p.mouseWheel = (event) => {
+        // Adjust scalar based on mouse wheel
+        scalar += event.delta * -0.01;
+        // Constrain the scalar to prevent too much zoom in or out
+        scalar = p.constrain(scalar, 1, 500);
+    }
 
-  p.draw = () => {
+    p.mousePressed = () => {
+        if (p.mouseButton == p.CENTER) {
+            previousMouseX = p.mouseX;
+            previousMouseY = p.mouseY;
+        }
+    }
 
-      // p.square(p.mouseX - p.width / 2, p.mouseY - p.height / 2, 200);
-      // p.circle(p.mouseX - p.width / 2, p.mouseY - p.height / 2, 100);
-      // p.rectangle(p.mouseX - p.width/2, p.mouseY - p.height/2, 10);    }
+    p.mouseDragged = () => {
+        //update buffer image location based on mouse drag of middle mouse click
+        if (p.mouseButton == p.CENTER) {
+            bufferX += p.mouseX - previousMouseX;
+            bufferY += p.mouseY - previousMouseY;
 
-    // p.circle(p.mouseX - p.width / 2, p.mouseY - p.height / 2, 100);
+            // update previous mouse positions
+            previousMouseX = p.mouseX;
+            previousMouseY = p.mouseY;
+        }
+    }
 
-    // Copy the rendered image into our prevFrame image
-    // Set the image of the previous frame into our shader
-    // p.push();
-    // p.shader(golShader);
-    shader.setUniform("tex", prevFrame);
-    shader.setUniform("cellWidth", cellWidth / p.width);
-    p.rect(-p.width / 2, -p.height / 2, p.width, p.height);
+    p.draw = () => {
+        p.background(backgroundColor);
 
-    prevFrame.image(p.get(), 0, 0);
+        // copy buffer into previousState unless it's the first run then keep initial state
+        if (!isFirstGeneration) {
+            previousState.image(buffer, 0, 0);
+        } else {
+            console.log('first generation');
+            isFirstGeneration = false;
+        }
 
-  };
+        // set previousState uniform for buffer
+        shader1Din1D.setUniform('previousState', previousState);
+        buffer.rect(
+            -buffer.width / 2,
+            buffer.height / 2,
+            buffer.width,
+            buffer.height
+        );
+
+        // begin buffer drawing process
+        p.translate(bufferX, bufferY);
+        p.scale(scalar);
+
+        // find coordinates to the center of the screen given scalar and webgl
+        let x = p.width / (2 * scalar) - buffer.width / 2;
+        let y = p.height / (2 * scalar) - buffer.height / 2;
+
+        // draw buffer image to main canvas
+        p.image(buffer, x, y);
+    }
 };
 
 export default oneInOneSketch;
