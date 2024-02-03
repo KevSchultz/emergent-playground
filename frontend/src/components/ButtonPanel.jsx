05697b33df69309953448644db220502e6dcde3f
Preston Nguyen <prhunguy@ucsc.edu>
2024-02-02 16:02:51 -0800

    small fix for double break in ver1

--- frontend/src/components/ButtonPanel.jsx
+++ frontend/src/components/ButtonPanel.jsx
@@ -21,7 +21,6 @@ function IconBrushButton({ sketch, icon, brush, setBrush }) {
         switch (icon) {
             case 'line':
                 document.body.style.cursor = 'crosshair';
-            break;
                 break;
             case 'square':
                 document.body.style.cursor = `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 100 100"><rect width="100" height="100" style="fill:none;stroke:white;stroke-width:2;"/></svg>'), auto`;
