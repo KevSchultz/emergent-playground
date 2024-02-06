6edf66b96a013a8f09d07fff0a20120984106804
KevSchultz <ksschultz02@gmail.com>
2024-02-06 12:09:34 -0800

    oneInOneSketch inside ViewerBuilderCreator.jsx

--- frontend/src/pages/ViewerBuilderCreator.jsx
+++ frontend/src/pages/ViewerBuilderCreator.jsx
@@ -10,23 +10,15 @@ import React from 'react';
 
 // Custom component imports
 import P5Wrapper from '../components/P5Wrapper';
-// import BackgroundLayer from '../components/BackgroundLayer';
 import UserInterfaceLayer from '../components/UserInterfaceLayer';
 import NavBar from '../components/NavBar';
 import ButtonPanel from '../components/ButtonPanel';
 import OptionsDrawer from '../components/OptionsDrawer';
 
 // Sketch imports (P5.js)
-// import gameOfLifeSketch from '../sketches/gameOfLife';
-// import bufferScalingSketch from '../sketches/bufferScalingSketch';
-// import gradientsMovingSketch from '../sketches/gradientsMovingSketch';
-// import ballMovingSketch from '../sketches/ballMovingSketch';
-// import particleSketch from '../sketches/particleSketch';
-// import oneInOneSketch from '../sketches/oneInOneSketch';
-// import randomColorsSketch from '../sketches/randomColorsSketch';
-// import gameOfLifeSketchZoomTmp from '../sketches/gameOfLifeSketchZoomTmp';
 import twoInTwo from '../sketches/twoInTwo';
 import cameraGameOfLife from '../sketches/cameraGameOfLife';
+import oneInOneSketch from '../sketches/oneInOneSketch';
 
 // Material UI imports
 import Button from '@mui/material/Button';
@@ -34,7 +26,7 @@ import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
 import Box from '@mui/material/Box';
 
-const sketches = [twoInTwo, cameraGameOfLife];
+const sketches = [twoInTwo, cameraGameOfLife, oneInOneSketch];
 
 /**
  * @component
